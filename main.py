e3d8e417990ed68719e4e4171aa878ef95a610c1
balazsborsos <b.blazs97@gmail.com>
2021-03-08 23:51:27 +0100

    clear board func added, removed started var

--- main.py
+++ main.py
@@ -196,15 +196,13 @@ def main(win, width):
     end = None
 
     run = True
-    started = False
 
     while run:
         draw(win, grid, ROWS, width)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if started:
-                continue
+
 
             if pygame.mouse.get_pressed()[0]:  # Left mouse button
                 pos = pygame.mouse.get_pos()
@@ -231,11 +229,16 @@ def main(win, width):
                     end = None
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and not started:
+                if event.key == pygame.K_SPACE and start and end:
                     for row in grid:
                         for node in row:
                             node.update_neighbors(grid)
                     a_star(lambda: draw(win, grid, ROWS, width), grid, start, end)
+
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = make_grid(ROWS, width)
 
     pygame.quit()
 
