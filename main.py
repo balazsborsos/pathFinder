54dc613c2fd89cd3755fb4c2224f2718b37d21c8
balazsborsos <b.blazs97@gmail.com>
2021-03-08 23:46:06 +0100

    reconstruct path functionality added

--- main.py
+++ main.py
@@ -95,6 +95,11 @@ def heuristic(p1, p2):
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
 
+def reconstruct_path(came_from, current, draw):
+    while current in came_from:
+        current = came_from[current]
+        current.make_path()
+        draw()
 
 def a_star(draw, grid, start, end):
     count = 0
@@ -117,6 +122,8 @@ def a_star(draw, grid, start, end):
         open_set_hash.remove(current)
 
         if current == end:
+            reconstruct_path(came_from, end, draw)
+            end.make_end()
             return True
 
         for neighbor in current.neighbors:
